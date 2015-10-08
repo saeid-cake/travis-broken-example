dd53b668bd37947dd51b64e8560e8446290ccc41
saeideng <saeideng@users.noreply.github.com>
2015-10-08 13:40:12 +0330

    Update Test.php

--- Test.php
+++ Test.php
@@ -1,16 +1,5 @@
 <?php
-        			
-        			
-        			
-        			
-        			
-        			
-        			
-        			
-        			
-        			
-        				
-class Test extends PHPUnit_Framework_TestCase
+    class Test extends PHPUnit_Framework_TestCase
 {
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);
